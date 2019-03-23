c7d5d58ad2221206bdae5e6a8f4517e6558b1230
Saprok123 <schpack.maks@gmail.com>
2019-03-23 22:19:19 +0300

    feat: fix code

--- src/index.js
+++ src/index.js
@@ -8,20 +8,19 @@ module.exports = function makeExchange(currency) {
     else if (currency < 0 ) {
         return {};
     }
-    if (currency > 0 && currency <= 10000){
+    if (currency > 0 && currency < 10000){
+        let ff ={};
         let Coin50 = Math.floor(currency / 50);
         let Coin25 = Math.floor((currency - (Coin50*50)) / 25);
         let Coin10 = Math.floor((currency - (Coin50*50) - (Coin25*25)) / 10);
         let Coin5 = Math.floor((currency - (Coin50*50) - (Coin25*25) - (Coin10*10)) / 5);
         let Coin1 = Math.floor((currency - (Coin50*50) - (Coin25*25) - (Coin10*10) - (Coin5*5)) / 1);
-        let H = Coin50 > 0 ? { 'H': Coin50 } : null
-        let Q = Coin25 > 0 ? { 'Q': Coin25 } : null
-        let D = Coin10 > 0 ? { 'D': Coin10 } : null
-        let N = Coin5 > 0 ? { 'N': Coin5 } : null
-        let P = Coin1 > 0 ? { 'P': Coin1 } : null
-        let message = {
-            H, Q, D, N, P
-        }
+        let h = Coin50 > 0 ?  {'H' : Coin50}  :{};
+        let q = Coin25 > 0 ? {'Q' : Coin25}  :{};
+        let d = Coin10 > 0 ? {'D' : Coin10} :{};
+        let n = Coin5 > 0 ? {'N' : Coin5} :{};
+        let p = Coin1 > 0 ? {'P' : Coin1} :{};
+        let message = {...h, ...q, ...d, ...n, ...p};
         return message
     }
 }
